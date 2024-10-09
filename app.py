33d55a459b11772d3baf73691312811f1bb89204
mgupta70 <mgupta70@asu.edu>
2024-10-09 10:42:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,10 @@ st.logo('media/asu_logo.png')
 # Load Data
 df = None
 # File uploader for a single .pkl file
-uploaded_file = st.file_uploader("Upload a .pkl file. Else download data by clicking [here](https://drive.google.com/file/d/14kFHUjzUk4uSXidZ6xM_g78gqiE_bvqk/view?usp=drive_link) 📥", type='pkl', accept_multiple_files=False)
+# uploaded_file = st.file_uploader("Upload a .pkl file. Else download data by clicking [here](https://drive.google.com/file/d/14kFHUjzUk4uSXidZ6xM_g78gqiE_bvqk/view?usp=drive_link) 📥", type='pkl', accept_multiple_files=False)
+
+uploaded_file = st.file_uploader("Upload a .pkl file. Link to data file has been shared with [Russell Genet] from srp) <a href="mailto:hello@streamlit.io">Contact us !</a> 📥", type='pkl', accept_multiple_files=False)
+
 
 if uploaded_file is not None:
     # Read the uploaded file into a DataFrame
