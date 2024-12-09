1b69e0fb3dd138ff34a5413942fa31ca8b7193aa
Chia-Ling Wei <86623375+ChialingWei@users.noreply.github.com>
2024-12-09 13:26:01 -0700

    Update 4 P2 Turbine Defect Inspection.py

--- pages/4 P2 Turbine Defect Inspection.py
+++ pages/4 P2 Turbine Defect Inspection.py
@@ -8,8 +8,11 @@ st.markdown('''
             If the data is collected during every maintenance, the differences among data can indicate the damage expansion based on location depth information. 
             Therefore, the damage can be monitored.''')
 
-            # FARO
-            # 360 camera
+col1, col2 = st.columns([0.5 , 1])
+with col1:
+            st.image("media/faro.png", caption="Fig 1. FARO Focus Laser Scanner")
+with col2:
+            st.image("media/titan360.png", caption="Fig 2. Insta360 Titan Camera")
             
 st.markdown('''The second part is building a damage detection system from previous inspection images. 
             The previous inspection images were distributed in the previous inspection log from the year 2013 to 2023. 
@@ -20,5 +23,10 @@ st.markdown('''The second part is building a damage detection system from previo
             The damage detection model was trained and provided for future inference. The inference process is demonstrated in the User Guide.
             ''')
 
-            # data
-            # pred
+col1, col2 = st.columns([0.5 , 1])
+with col1:
+    st.image("media/dataset.png", caption="Fig 3. (a) Example Damage Image")
+
+with col2:
+    st.image("media/prediction.png", caption="(b) Damage Prediction Result")
+
